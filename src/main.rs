9daa15beac8581e397af708cd33a225ababb3aeb
JyJyJcr <82190170+JyJyJcr@users.noreply.github.com>
2024-02-12 01:54:07 +0900

    fix: set `ConsoleAppender` to use stderr

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod client;
 mod server;
 
-use log4rs::append::console::ConsoleAppender;
+use log4rs::append::console::{ConsoleAppender, Target};
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config, Root};
 use log4rs::encode::pattern::PatternEncoder;
@@ -52,11 +52,12 @@ fn main() {
                 .unwrap()
         }
         None => {
-            let stdout = ConsoleAppender::builder()
+            let stderr = ConsoleAppender::builder()
                 .encoder(Box::<PatternEncoder>::default())
+                .target(Target::Stderr)
                 .build();
             Config::builder()
-                .appender(Appender::builder().build("stderr", Box::new(stdout)))
+                .appender(Appender::builder().build("stderr", Box::new(stderr)))
                 .build(Root::builder().appender("stderr").build(level))
                 .unwrap()
         }
